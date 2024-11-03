9873738b505c86e0fb92fd9139358027890d8b3d
ipeksnmz <91756562+ipeksnmz@users.noreply.github.com>
2024-11-03 22:32:08 +0300

    Update code.py

--- code.py
+++ code.py
@@ -1,7 +1,7 @@
 # CS 421 Programming Assignment 1 - PseudoGit
 
 def greet_the_student():
-    return "22103939 Ipek Sonmez"
+    return "22103939 Ipek Sonmez Usta Shifu"
 
 if(__name__ == "__main__"):
     print(greet_the_student())
